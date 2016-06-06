0cfa2c6714c0ff2ce4c04f95ae36600eb2e3ddc9
BlairMcMillan <theblairmcmillan@gmail.com>
2016-06-06 15:47:47 -0500

    cleaning up home screen and lists

--- weather/www/js/app.js
+++ weather/www/js/app.js
@@ -38,15 +38,6 @@ $stateProvider
           controller: 'LocationsCtrl'
         }
       }
-    })
-    .state('tab.settings', {
-      url: '/settings',
-      views: {
-        'tab-settings': {
-          templateUrl: 'templates/tab-settings.html',
-          controller: 'SettingsCtrl'
-        }
-      }
     });
 // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/home');
